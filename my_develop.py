44f624391e271bd2cac6757e6ea4bc986e270583
speecher <hit_master@163.com>
2018-08-13 15:55:56 +0800

    修改代码，加入特征提取模块

--- my_develop.py
+++ my_develop.py
@@ -1,12 +1,11 @@
 from collections import Counter
-
-
-
+import scipy.io.wavfile as wav
+from python_speech_features import mfcc
+import matplotlib.pyplot as plt
 
 def dict_test():
 	all_word = []
 	label = []
-
 	# python字典生成方法，没有语音，只有汉字对应了序号
 	label = ['hello','hello','hello','hi','hi','hi','you', 'you']
 	all_word += [word for word in label]
@@ -14,7 +13,6 @@ def dict_test():
 	words = sorted(counter)
 	words_size = len(words)
 	word_num_map = dict(zip(words, range(words_size)))
-
 	print('all words i use :\n',all_word)
 	print('\nafter counter:\n',counter)
 	print('\nafter sort:\n',words)
@@ -22,11 +20,18 @@ def dict_test():
 	print('\nthe words num map:\n',word_num_map)
 
 
+def readwav(audio_filename, n_input=26):
+	# 读取音频文件
+	fs, audio = wav.read(audio_filename)
+    # 获取mfcc数值
+	orig_inputs = mfcc(audio, samplerate=fs, numcep=26)
+	plt.plot(orig_inputs)
+	plt.show()
+
+
 def main():
-	pass
-
-
+	readwav('./wav/test.wav')
+	
 
 if __name__ == '__main__':
 	main()
-	dict_test()
